6531547ec2a9e3d70795f12815ebf2cfb2b2673d
chen1125he <1162925744@qq.com>
2019-01-01 17:01:35 +0800

    skip admin auth

--- app/controllers/api/admin/base_controller.rb
+++ app/controllers/api/admin/base_controller.rb
@@ -2,5 +2,4 @@ class API::Admin::BaseController < API::BaseController
   include AdminAuthentication
 
   skip_before_action :authenticate_user!
-  skip_before_action :ensure_authenticated_admin
 end
