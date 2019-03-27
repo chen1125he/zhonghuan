3f8c4f6e56d48b4cc9e26ab8f043b80bf1f91a6b
chen1125he <1162925744@qq.com>
2019-03-27 09:40:23 +0800

    fix image order

--- app/models/building_display_picture.rb
+++ app/models/building_display_picture.rb
@@ -15,7 +15,7 @@
 
 class BuildingDisplayPicture < ApplicationRecord
   belongs_to :building_display, optional: true
-  has_many :pictures, -> { where(special_type: nil) }, as: :owner, class_name: 'Image', dependent: :destroy
+  has_many :pictures, -> { where(special_type: nil).order(id: :asc) }, as: :owner, class_name: 'Image', dependent: :destroy
 
   accepts_nested_attributes_for :pictures, allow_destroy: true
 
